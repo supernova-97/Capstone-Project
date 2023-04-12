6b9319db2cf1735c1fcba6769d0dbd71a277ae31
Felix Buyni <felix.buyni@outlook.de>
2023-04-12 16:00:09 +0200

    styling

--- pages/journal/[journalID].js
+++ pages/journal/[journalID].js
@@ -1,4 +1,6 @@
 import { useRouter } from "next/router";
+import Link from "next/link";
+import styled from "styled-components";
 
 function JournalDetail({ journals }) {
   const router = useRouter();
@@ -16,11 +18,57 @@ function JournalDetail({ journals }) {
 
   return (
     <>
-      <h1>{name}</h1>
-      <h2>{destination}</h2>
-      <p>{description}</p>
+      <EntryWrapper>
+        <Heading>{name}</Heading>
+        <Destination>in {destination}</Destination>
+        <Description>&quot;{description}&quot;</Description>
+      </EntryWrapper>
+      <BackToJournalsButton href="/journal">
+        back to Journals
+      </BackToJournalsButton>
     </>
   );
 }
 
 export default JournalDetail;
+
+const EntryWrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Heading = styled.h1`
+  font-size: 2.2rem;
+  margin-bottom: 0;
+  background-color: #d9ff80;
+  padding: 10px;
+  width: 30%;
+  text-align: center;
+  border: 2px solid black;
+  box-shadow: -4px 4px black;
+`;
+
+const Destination = styled.h2`
+  background-color: #d9ff80;
+  padding: 5px 15px;
+`;
+
+const Description = styled.p`
+  font-size: 1.1rem;
+`;
+
+const BackToJournalsButton = styled(Link)`
+  background-color: #d9ff80;
+  border: 2px solid black;
+  box-shadow: -2px 2px black;
+  margin: 45px 10px 0 10px;
+  padding: 5px;
+  width: fit-content;
+  text-decoration: none;
+  color: black;
+  transition: 0.2s ease-out;
+  :hover {
+    background-color: #db9d47;
+  }
+`;
--- pages/journal/index.js
+++ pages/journal/index.js
@@ -8,19 +8,17 @@ function Journal({ journalData, journals }) {
         <h1>Journal</h1>
         <EntriesWrapper>
           {journals.length > 0 ? (
-            <ul>
+            <>
               {journals.map((journal) => (
-                <p key={journal.id}>
-                  <Link href={`/journal/${journal.id}`}>
-                    {journal.name} in {journal.destination}
-                  </Link>
-                </p>
+                <JournalLink key={journal.id} href={`/journal/${journal.id}`}>
+                  {journal.name} in {journal.destination}
+                </JournalLink>
               ))}
-            </ul>
+            </>
           ) : (
             "No Journals yet. Create one!"
           )}
-          <Link href="/journal/form">New Journal</Link>
+          <NewJournalButton href="/journal/form">New Journal</NewJournalButton>
         </EntriesWrapper>
         <DashboardLink href="/">Dashboard</DashboardLink>
       </JournalWrapper>
@@ -40,9 +38,41 @@ const EntriesWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  border: 2px solid black;
+  padding: 30px;
+  box-shadow: -3px 3px black;
 `;
 
 const DashboardLink = styled(Link)`
+  background-color: #d9ff80;
+  border: 2px solid black;
+  box-shadow: -2px 2px black;
+  margin: 25px 10px 0 10px;
+  padding: 5px;
+  width: fit-content;
+  text-decoration: none;
+  color: black;
+  transition: 0.2s ease-out;
+  :hover {
+    background-color: #db9d47;
+  }
+`;
+
+const JournalLink = styled(Link)`
+  background-color: #d9ff80;
+  color: black;
+  text-decoration: none;
+  box-shadow: -2px 2px black;
+  padding: 5px 15px;
+
+  margin: 15px;
+  transition: 0.3s;
+  :hover {
+    background-color: #db9d47;
+  }
+`;
+
+const NewJournalButton = styled(Link)`
   background-color: #d9ff80;
   border: 2px solid black;
   box-shadow: -2px 2px black;
